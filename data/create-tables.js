6c8ece8d3d76f5d1f7ae628249a0d39859c7f0fe
RACHEL DONAHUE <RACHEL.INGOODHEALTH@GMAIL.COM>
2020-05-12 14:32:57 -0700

    more fie setup and updates data files

--- data/create-tables.js
+++ data/create-tables.js
@@ -16,11 +16,12 @@ async function run() {
                     email VARCHAR(256) NOT NULL,
                     hash VARCHAR(512) NOT NULL
                 );           
-                CREATE TABLE animals (
+                CREATE TABLE todos (
                     id SERIAL PRIMARY KEY NOT NULL,
                     name VARCHAR(512) NOT NULL,
-                    cool_factor INTEGER NOT NULL,
-                    owner_id INTEGER NOT NULL REFERENCES users(id)
+                    priority INTEGER NOT NULL,
+                    completed BOOLEAN DEFAULT FALSE,
+                    owner_id INTEGER NOT NULL REFERENCES users(id
             );
         `);
 
---
+++ data/todos.js
@@ -0,0 +1,26 @@
+module.exports = [
+  {
+    name: 'do laundry',
+    priority: 3
+  },
+  {
+    name: 'grocery shopping',
+    priority: 1
+  },
+  {
+    name: 'wash dishes',
+    priority: 3
+  },
+  {
+    name: 'pay bills',
+    priority: 2
+  },
+  {
+    name: 'sweep floors',
+    priority: 4
+  },
+  {
+    name: 'take out trash',
+    priority: 4
+  }
+];
